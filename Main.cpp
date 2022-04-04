5cffee2b83da2158d338eaff275db4a2a19f38dc
Nimeryon <t.auriol46@gmail.com>
2022-04-04 23:18:39 +0200

    Config variables update

--- Main.cpp
+++ Main.cpp
@@ -9,9 +9,9 @@ int WINDOW_WIDTH = 1080;
 int WINDOW_HEIGHT = 720;
 
 // Elements
-maoutch::Vector2 ELEMENT_SIZE = { 34 };
+maoutch::Vector2 ELEMENT_SIZE = { 38 };
 maoutch::Vector2 ELEMENT_SCALE = { 1.25f };
-float ELEMENT_SNAP_DISTANCE = 2.f;
+float ELEMENT_SNAP_DISTANCE = 2;
 float ELEMENT_DT_MULTIPLIER = 10;
 float ELEMENT_SWIPE_DISTANCE = 20;
 
