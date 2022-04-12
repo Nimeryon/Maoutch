6893852957b6be8eaaa39cf32545da2783e04260
Nimeryon <t.auriol46@gmail.com>
2022-04-12 15:49:40 +0200

    Using config file

--- Game/Objects/MatchHint.cpp
+++ Game/Objects/MatchHint.cpp
@@ -1,39 +1,37 @@
 #include "MatchHint.h"
 #include "../Match/MatchFinder.h"
 #include "../Objects/MatchGrid.h"
-#include "../../Engine/AssetLoader.h"
-
-extern maoutch::Vector2 ELEMENT_SIZE;
+#include "../../Engine/Assets.h"
 
 namespace maoutch
 {
 	MatchHint::MatchHint() :
 		GameObject("MatchHint", 2, true, false),
 		_hintAnimationNorth(
-			_animationTime,
-			&AssetLoader::GetInstance()->GetTexture("Hint Arrow Animation"),
-			Vector2i(ELEMENT_SIZE.x, ELEMENT_SIZE.y * 2),
+			Assets::Config<float>("Hint", "AnimationTime"),
+			&Assets::GetInstance()->GetTexture("Hint Arrow Animation"),
+			Vector2i(Assets::Config<float>("Element", "Size"), Assets::Config<float>("Element", "Size") * 2),
 			Vector2i(3, 0),
 			false
 		),
 		_hintAnimationEast(
-			_animationTime,
-			&AssetLoader::GetInstance()->GetTexture("Hint Arrow Animation"),
-			Vector2i(ELEMENT_SIZE.x * 2, ELEMENT_SIZE.y),
+			Assets::Config<float>("Hint", "AnimationTime"),
+			&Assets::GetInstance()->GetTexture("Hint Arrow Animation"),
+			Vector2i(Assets::Config<float>("Element", "Size") * 2, Assets::Config<float>("Element", "Size")),
 			Vector2i::Zero(),
 			false
 		),
 		_hintAnimationSouth(
-			_animationTime,
-			&AssetLoader::GetInstance()->GetTexture("Hint Arrow Animation"),
-			Vector2i(ELEMENT_SIZE.x, ELEMENT_SIZE.y * 2),
+			Assets::Config<float>("Hint", "AnimationTime"),
+			&Assets::GetInstance()->GetTexture("Hint Arrow Animation"),
+			Vector2i(Assets::Config<float>("Element", "Size"), Assets::Config<float>("Element", "Size") * 2),
 			Vector2i(2, 0),
 			false
 		),
 		_hintAnimationWest(
-			_animationTime,
-			&AssetLoader::GetInstance()->GetTexture("Hint Arrow Animation"),
-			Vector2i(ELEMENT_SIZE.x * 2, ELEMENT_SIZE.y),
+			Assets::Config<float>("Hint", "AnimationTime"),
+			&Assets::GetInstance()->GetTexture("Hint Arrow Animation"),
+			Vector2i(Assets::Config<float>("Element", "Size") * 2, Assets::Config<float>("Element", "Size")),
 			Vector2i(0, 1),
 			false
 		)
@@ -71,25 +69,26 @@ namespace maoutch
 
 		_StopAnimations();
 
+		const float elementSize = Assets::Config<float>("Element", "Size");
 		switch (_possibleMatch.direction)
 		{
-			case Direction::DirectionValue::NORTH:
-				SetOrigin(Vector2(ELEMENT_SIZE.x / 2.f, ELEMENT_SIZE.y + ELEMENT_SIZE.y / 2.f));
+			case Direction::DirectionValue::North:
+				SetOrigin(Vector2(elementSize / 2.f, elementSize + elementSize / 2.f));
 				_hintAnimationNorth.Play();
 				break;
 
-			case Direction::DirectionValue::EAST:
-				SetOrigin(Vector2(ELEMENT_SIZE.x / 2.f, ELEMENT_SIZE.y / 2.f));
+			case Direction::DirectionValue::East:
+				SetOrigin(Vector2(elementSize / 2.f, elementSize / 2.f));
 				_hintAnimationEast.Play();
 				break;
 
-			case Direction::DirectionValue::SOUTH:
-				SetOrigin(Vector2(ELEMENT_SIZE.x / 2.f, ELEMENT_SIZE.y / 2.f));
+			case Direction::DirectionValue::South:
+				SetOrigin(Vector2(elementSize / 2.f, elementSize / 2.f));
 				_hintAnimationSouth.Play();
 				break;
 
-			case Direction::DirectionValue::WEST:
-				SetOrigin(Vector2(ELEMENT_SIZE.x + ELEMENT_SIZE.x / 2.f, ELEMENT_SIZE.y / 2.f));
+			case Direction::DirectionValue::West:
+				SetOrigin(Vector2(elementSize + elementSize / 2.f, elementSize / 2.f));
 				_hintAnimationWest.Play();
 				break;
 
@@ -108,19 +107,19 @@ namespace maoutch
 	{
 		switch (_possibleMatch.direction)
 		{
-		case Direction::DirectionValue::NORTH:
+		case Direction::DirectionValue::North:
 			_hintAnimationNorth.Draw(window, transform);
 			break;
 
-		case Direction::DirectionValue::EAST:
+		case Direction::DirectionValue::East:
 			_hintAnimationEast.Draw(window, transform);
 			break;
 
-		case Direction::DirectionValue::SOUTH:
+		case Direction::DirectionValue::South:
 			_hintAnimationSouth.Draw(window, transform);
 			break;
 
-		case Direction::DirectionValue::WEST:
+		case Direction::DirectionValue::West:
 			_hintAnimationWest.Draw(window, transform);
 			break;
 
