11d8533bc61c2a1a548f0bd64598742f8f67d2bd
Nimeryon <t.auriol46@gmail.com>
2022-04-12 15:50:30 +0200

    delete global variable > using config file

--- Main.cpp
+++ Main.cpp
@@ -2,16 +2,6 @@
 #include <time.h>
 
 #include "Game/Game.h"
-#include "Types/Vector2i.h"
-
-// Window
-int WINDOW_WIDTH = 1280;
-int WINDOW_HEIGHT = 720;
-
-// Elements
-maoutch::Vector2 ELEMENT_SIZE = { 38 };
-maoutch::Vector2 ELEMENT_SCALE = { 1.4f };
-float ELEMENT_SWIPE_DISTANCE = 30;
 
 int main()
 {
