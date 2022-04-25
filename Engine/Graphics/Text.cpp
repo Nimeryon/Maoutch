c23aa2272487ab7812fb2c116e2a48bd56ab6cb6
Nimeryon <t.auriol46@gmail.com>
2022-04-25 12:15:05 +0200

    Text

---
+++ Engine/Graphics/Text.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+
+#include "Text.h"
+#include "../Assets.h"
+
+namespace maoutch
+{
+	Text::Text(const std::string& text) :
+		_text(new sf::Text())
+	{
+		_text->setFont(Assets::Instance()->GetFont());
+		SetText(text);
+
+		Assets::Instance()->onFontChange += EventHandler::Bind<sf::Font&>(&Text::_SetFont, this);
+	}
+	Text::~Text()
+	{
+		Assets::Instance()->onFontChange -= EventHandler::Bind<sf::Font&>(&Text::_SetFont, this);
+	}
+	
+	void Text::SetText(const std::string& text) { _text->setString(text); }
+	void Text::SetSize(const int& size) { _text->setCharacterSize(size); }
+	void Text::SetLineSpacing(const float& spacing) { _text->setLineSpacing(spacing); }
+	void Text::SetLetterSpacing(const float& spacing) { _text->setLetterSpacing(spacing); }
+	void Text::SetColor(const sf::Color& color) { _text->setFillColor(color); }
+	void Text::SetOutlineThickness(const float& thickness) { _text->setOutlineThickness(thickness); }
+	void Text::SetOutlineColor(const sf::Color& color) { _text->setOutlineColor(color); }
+
+	std::string Text::GetText() const { return _text->getString(); }
+	int Text::GetSize() const { return _text->getCharacterSize(); }
+	float Text::GetLineSpacing() const { return _text->getLineSpacing(); }
+	float Text::GetLetterSpacing() const { return _text->getLetterSpacing(); }
+	sf::Color Text::GetColor() const { return _text->getFillColor(); }
+	float Text::GetOutlineThickness() const { return _text->getOutlineThickness(); }
+	sf::Color Text::GetOutlineColor() const { return _text->getOutlineColor(); }
+
+	void Text::Draw(sf::RenderWindow& window, const sf::Transform& transform)
+	{
+		sf::Transform t = transform * _localTransform.getTransform();
+		window.draw(*_text, t);
+	}
+
+	void Text::_SetFont(sf::Font& font) { _text->setFont(font); }
+}
---
+++ Engine/Graphics/Text.h
@@ -0,0 +1,46 @@
+#pragma once
+namespace sf
+{
+	class Font;
+}
+#include <SFML/Graphics/Text.hpp>
+
+#include "../../Interfaces/ITransformable.h"
+#include "../Objects/GameObject.h"
+
+namespace maoutch
+{
+	class Text : public ITransformable
+	{
+	public:
+		explicit Text(const std::string& text = "");
+		virtual ~Text();
+
+		// Setters
+
+		void SetText(const std::string& text);
+		void SetSize(const int& size);
+		void SetLineSpacing(const float& spacing);
+		void SetLetterSpacing(const float& spacing);
+		void SetColor(const sf::Color& color);
+		void SetOutlineThickness(const float& thickness);
+		void SetOutlineColor(const sf::Color& color);
+
+		// Getters
+
+		std::string GetText() const;
+		int GetSize() const;
+		float GetLineSpacing() const;
+		float GetLetterSpacing() const;
+		sf::Color GetColor() const;
+		float GetOutlineThickness() const;
+		sf::Color GetOutlineColor() const;
+
+		void Draw(sf::RenderWindow& window, const sf::Transform& transform);
+
+	private:
+		sf::Text* _text;
+
+		void _SetFont(sf::Font& font);
+	};
+}
