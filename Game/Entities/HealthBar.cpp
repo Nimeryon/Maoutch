ce2e8657c028524525385ca88b8a33c6b76c2335
Nimeryon <t.auriol46@gmail.com>
2022-04-25 12:20:13 +0200

    HealthBar Update

--- Game/Entities/HealthBar.cpp
+++ Game/Entities/HealthBar.cpp
@@ -5,26 +5,26 @@
 #include "../../Tools/Colors.h"
 #include "../../Tools/Math.h"
 #include "../../Engine/Assets.h"
+#include "../Effects/Shaker.h"
 #include "HealthBarSkull.h"
 
 namespace maoutch
 {
-	HealthBarPart::HealthBarPart(Sprite* sprite, const Vector2i& offset) :
-		sprite(sprite)
-	{
-		transform.move(offset);
-	}
-
 	HealthBar::HealthBar(const float& health, const bool& isBossBar, int width) :
 		GameObject("HealthBar", 100),
+		_backgroundRectangle(new sf::RectangleShape()),
+		_damageIndicatorRectangle(new sf::RectangleShape()),
 		_healthRectangle(new sf::RectangleShape()),
+		_healthText(new Text(std::to_string(health))),
 		_currentHealth(health),
 		_health(health),
 		_isBossBar(isBossBar),
-		_breathIn(false),
-		_breathScale(Assets::Config<float>("HealthBar", "BreathScale")),
-		_currentBreathTime(0),
-		_breathTime(Assets::Config<float>("HealthBar", "BreathTime"))
+		_indicatorHealth(health),
+		_currentIndicatorTime(0),
+		_indicatorTime(Assets::Config<float>("HealthBar", "IndicatorTime")),
+		_startedIndicating(false),
+		_isIndicating(false),
+		_indicatorTimer(Assets::Config<float>("HealthBar", "IndicatorDelay"), &HealthBar::_OnIndicatorTimer, this)
 	{
 		Vector2 partSize = Vector2(
 			Assets::Config<float>("HealthBar", "PartSize"),
@@ -41,66 +41,104 @@ namespace maoutch
 			if (x == 0) side = 0;
 			else if (x == width - 1) side = 2;
 
-			_decorators.emplace_back(HealthBarPart(
-				new Sprite(
-					&Assets::GetInstance()->GetTexture("HealthBar"),
-					Vector2i::From(partSize),
-					Vector2i(side, 0)
-				),
-				Vector2i(partSize.x * x, 0)
+			_decorators.emplace_back(new Sprite(
+				&Assets::Instance()->GetTexture("HealthBar"),
+				Vector2i::From(partSize),
+				Vector2i(side, 0)
 			));
+			_decorators[x]->Move(Vector2i(partSize.x * x, 0));
 		}
 
-		_healthRectangle->setSize(Vector2(_size.x - 6, _size.y - 6));
-		_healthRectangle->setOrigin(-3, -3);
+		// Health rectangles
+		const Vector2 size = Vector2(_size.x - 6, _size.y - 6);
+		const Vector2 origin = Vector2(-3, -3);
+		_backgroundRectangle->setSize(size);
+		_backgroundRectangle->setOrigin(origin);
+		_backgroundRectangle->setFillColor(sf::Color(66, 66, 66));
+
+		_damageIndicatorRectangle->setSize(size);
+		_damageIndicatorRectangle->setOrigin(origin);
+		_damageIndicatorRectangle->setFillColor(sf::Color::Red);
+
+		_healthRectangle->setSize(size);
+		_healthRectangle->setOrigin(origin);
 		_healthRectangle->setFillColor(sf::Color::Green);
 
+		// Health skull
 		HealthBarSkull* skull = new HealthBarSkull();
-		skull->SetOrigin(Vector2(Assets::Config<float>("HealthBar", "SkullSize") / 2.f, Assets::Config<float>("HealthBar", "SkullOriginY")));
 		skull->SetPosition(Vector2(_size.x / 2.f, 0));
 		AddChildren(skull);
-		
+
+		// Health Text
+		_healthText->SetColor(sf::Color::White);
+		_healthText->SetOutlineColor(sf::Color::Black);
+		_healthText->SetOutlineThickness(1);
+		_healthText->SetSize(16);
+
+		// Boss
 		SetIsBossBar(_isBossBar);
 	}
 	HealthBar::~HealthBar()
 	{
 		delete _healthRectangle;
 		for (auto& decorator : _decorators)
-			delete decorator.sprite;
+			delete decorator;
 	}
 
 	void HealthBar::FixedUpdate(float dt)
 	{
-		_currentBreathTime += dt;
-
-		const float t = _currentBreathTime / _breathTime;
-		if (_breathIn) SetScale(_scale + _breathScale * Ease(easing::EaseType::EaseInSine, t));
-		else SetScale(_scale + _breathScale * Ease(easing::EaseType::EaseOutSine, 1 - t));
-			
-		if (_currentBreathTime >= _breathTime)
+		if (_isIndicating)
 		{
-			_currentBreathTime -= _breathTime;
-			_breathIn = !_breathIn;
+			_currentIndicatorTime += dt;
+			if (_currentIndicatorTime >= _indicatorTime)
+			{
+				_currentIndicatorTime = _indicatorTime;
+				_isIndicating = false;
+				_startedIndicating = false;
+
+				// Events
+				if (_currentHealth == 0) onDeath();
+			}
+
+			const float timerT = _currentIndicatorTime / _indicatorTime;
+			_indicatorHealth = math::Lerp(_indicatorHealth, _currentHealth, Ease(easing::EaseType::EaseInExpo, timerT));
+			const float t = _indicatorHealth / _health;
+			_damageIndicatorRectangle->setSize(Vector2(math::Clamp(_size.x * t - 6, 0.f, _size.x), _size.y - 6));
 		}
 	}
-	
+
 	void HealthBar::Damage(const float& damage)
 	{
-		if (_currentHealth - damage <= 0.f)
-		{
-			_currentHealth = _health;
-		}
+		_startedIndicating = true;
+		_indicatorTimer.Restart();
 
-		_currentHealth -= damage;
+		// Update health
+		_currentHealth = math::Clamp(_currentHealth += damage, 0.f, _health);
 
 		const float t = _currentHealth / _health;
 		_healthRectangle->setSize(Vector2(math::Clamp(_size.x * t - 6, 0.f, _size.x), _size.y - 6));
 		_healthRectangle->setFillColor(colors::LerpRGB(sf::Color::Red, sf::Color::Green, t, easing::EaseType::EaseInOutQuad));
+
+		_healthText->SetText(std::to_string(_currentHealth));
+
+		// Events
+		if (damage > 0) onHeal(damage);
+		else
+		{
+			Shaker::Instance()->Apply({
+				this,
+				0.25,
+				2,
+				150,
+				150
+			});
+			onDamage(damage);
+		}
 	}
 	void HealthBar::SetIsBossBar(const bool& isBossBar)
 	{
 		_isBossBar = isBossBar;
-		GetChildren("HealthBar Skull")->SetActive(_isBossBar);
+		GetChildren<HealthBarSkull>("HealthBar Skull")->SetActive(_isBossBar);
 
 		_scale = _isBossBar
 			? Assets::Config<float>("HealthBar", "BossBarScale")
@@ -108,11 +146,22 @@ namespace maoutch
 		SetScale(_scale);
 	}
 
+	void HealthBar::_OnIndicatorTimer()
+	{
+		// Setup Indicator
+		_isIndicating = true;
+		_currentIndicatorTime = 0;
+	}
+
 	void HealthBar::_OnDraw(sf::RenderWindow& window, const sf::Transform& transform)
 	{
+		window.draw(*_backgroundRectangle, transform);
+		if (_startedIndicating) window.draw(*_damageIndicatorRectangle, transform);
 		window.draw(*_healthRectangle, transform);
 		
 		for (auto& decorator : _decorators)
-			decorator.sprite->Draw(window, transform * decorator.transform.getTransform());
+			decorator->Draw(window, transform);
+
+		_healthText->Draw(window, transform);
 	}
 }
--- Game/Entities/HealthBar.h
+++ Game/Entities/HealthBar.h
@@ -7,18 +7,14 @@ namespace sf
 
 #include "../../Engine/Objects/GameObject.h"
 #include "../../Engine/Graphics/Sprite.h"
+#include "../../Engine/Graphics/Text.h"
+#include "../../Interfaces/IDamageable.h"
+#include "../../Tools/Event.h"
+#include "../../Engine/Tools/Timer.h"
 
 namespace maoutch
 {
-	struct HealthBarPart
-	{
-		HealthBarPart(Sprite* sprite, const Vector2i& offset);
-
-		Sprite* sprite;
-		sf::Transformable transform;
-	};
-
-	class HealthBar : public GameObject
+	class HealthBar : public GameObject, public IDamageable
 	{
 	public:
 		HealthBar(const float& health, const bool& isBossBar = false, int width = 3);
@@ -26,23 +22,35 @@ namespace maoutch
 
 		void FixedUpdate(float dt) override;
 
-		void Damage(const float& damage);
+		void Damage(const float& damage) override;
 		void SetIsBossBar(const bool& isBossBar);
 
+		Event<void> onDeath;
+		Event<const float> onDamage;
+		Event<const float> onHeal;
+
 	private:
+		sf::RectangleShape* _backgroundRectangle;
+		sf::RectangleShape* _damageIndicatorRectangle;
 		sf::RectangleShape* _healthRectangle;
-		std::vector<HealthBarPart> _decorators;
+		std::vector<Sprite*> _decorators;
+		Text* _healthText;
 
 		Vector2 _size;
+		float _scale;
 		float _currentHealth;
 		float _health;
 		bool _isBossBar;
-		float _scale;
 
-		bool _breathIn;
-		float _breathScale;
-		float _currentBreathTime;
-		float _breathTime;
+		float _indicatorHealth;
+		float _currentIndicatorTime;
+		float _indicatorTime;
+		bool _startedIndicating;
+		bool _isIndicating;
+
+		Timer<HealthBar> _indicatorTimer;
+
+		void _OnIndicatorTimer();
 
 		void _OnDraw(sf::RenderWindow& window, const sf::Transform& transform) override;
 	};
