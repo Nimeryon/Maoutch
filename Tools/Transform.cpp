54d1795348bd742f8158f3b688d45209e8a07aae
Nimeryon <t.auriol46@gmail.com>
2022-05-03 12:08:15 +0200

    Use Itransformable GetBounds

--- Tools/Transform.cpp
+++ Tools/Transform.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
+
 #include "Transform.h"
+#include "../Interfaces/ITransformable.h"
 
 namespace maoutch
 {
@@ -26,24 +28,17 @@ namespace maoutch
 
 			return rect;
 		}
-
-		bool Contains(const sf::Shape& shape, const sf::Transformable& transform, const Vector2& point)
+		
+		bool Contains(const ITransformable& transformable, const sf::Transformable& transform, const Vector2& point)
 		{
-			return GetRect(shape.getGlobalBounds(), transform).contains(point);
-		}
-		bool Contains(const sf::Sprite& sprite, const sf::Transformable& transform, const Vector2& point)
-		{
-			return GetRect(sprite.getGlobalBounds(), transform).contains(point);
+			return GetRect(transformable.GetBounds(), transform).contains(point);
 		}
 
-		bool Intersects(const sf::Shape& shape1, const sf::Transformable& transform1, const sf::Shape& shape2, const sf::Transformable& transform2)
+		bool Intersects(const ITransformable& transformable1, const sf::Transformable& transform1, const ITransformable& transformable2, const sf::Transformable& transform2)
 		{
-			return GetRect(shape1.getGlobalBounds(), transform1).intersects(GetRect(shape2.getGlobalBounds(), transform2));
+			return GetRect(transformable1.GetBounds(), transform1).intersects(GetRect(transformable2.GetBounds(), transform2));
 		}
-		bool Intersects(const sf::Sprite& sprite1, const sf::Transformable& transform1, const sf::Sprite& sprite2, const sf::Transformable& transform2)
-		{
-			return GetRect(sprite1.getGlobalBounds(), transform1).intersects(GetRect(sprite2.getGlobalBounds(), transform2));
-		}
+
 
 		std::array<sf::Vertex, 4> GetVertices(const sf::FloatRect& rect)
 		{
--- Tools/Transform.h
+++ Tools/Transform.h
@@ -2,8 +2,6 @@
 namespace sf
 {
 	class Vertex;
-	class Shape;
-	class Sprite;
 	class Transformable;
 }
 #include <array>
@@ -12,17 +10,17 @@ namespace sf
 
 namespace maoutch
 {
+	class ITransformable;
+
 	namespace transform
 	{
 		sf::Transformable Compose(const sf::Transformable& target, const sf::Transformable& from);
 
 		sf::FloatRect GetRect(sf::FloatRect rect, const sf::Transformable& transform);
 
-		bool Contains(const sf::Shape& shape, const sf::Transformable& transform, const Vector2& point);
-		bool Contains(const sf::Sprite& sprite, const sf::Transformable& transform, const Vector2& point);
+		bool Contains(const ITransformable& transformable, const sf::Transformable& transform, const Vector2& point);
 
-		bool Intersects(const sf::Shape& shape1, const sf::Transformable& transform1, const sf::Shape& shape2, const sf::Transformable& transform2);
-		bool Intersects(const sf::Sprite& sprite1, const sf::Transformable& transform1, const sf::Sprite& sprite2, const sf::Transformable& transform2);
+		bool Intersects(const ITransformable& transformable1, const sf::Transformable& transform1, const ITransformable& transformable2, const sf::Transformable& transform2);
 
 		std::array<sf::Vertex, 4> GetVertices(const sf::FloatRect& rect);
 	}
