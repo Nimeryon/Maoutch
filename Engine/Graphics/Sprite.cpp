c7cc3ada61b4d30d5a89424139e5b486d8238616
Nimeryon <t.auriol46@gmail.com>
2022-05-03 12:15:12 +0200

    Added GetBounds and Color properties

--- Engine/Graphics/Sprite.cpp
+++ Engine/Graphics/Sprite.cpp
@@ -1,14 +1,16 @@
 #include <array>
-#include "Sprite.h"
 #include <SFML/Graphics/RenderWindow.hpp>
 
 #include "../../Tools/Texture.h"
+#include "../../Tools/Transform.h"
+#include "Sprite.h"
 
 namespace maoutch
 {
 	Sprite::Sprite(const sf::Texture* texture, const Vector2i& frameSize, const Vector2i& framePosition) :
+		_frameSize(frameSize),
 		_framePosition(framePosition),
-		_frameSize(frameSize)
+		_color(sf::Color::White)
 	{
 		_renderState.texture = texture;
 		_vertexArray.setPrimitiveType(sf::Quads);
@@ -17,6 +19,11 @@ namespace maoutch
 	}
 	Sprite::~Sprite() = default;
 
+	void Sprite::SetColor(const sf::Color& color)
+	{
+		_color = color;
+		_SetFrame();
+	}
 	void Sprite::SetFramePosition(const Vector2i& framePosition)
 	{
 		_framePosition = framePosition;
@@ -28,25 +35,27 @@ namespace maoutch
 		_SetFrame();
 	}
 
+	sf::Color Sprite::GetColor() const { return _color; }
+
 	void Sprite::Draw(sf::RenderWindow& window, const sf::Transform& transform)
 	{
 		_renderState.transform = transform * _localTransform.getTransform();
 		window.draw(_vertexArray, _renderState);
 	}
+	
+	sf::FloatRect Sprite::GetBounds() const	{ return _vertexArray.getBounds(); }
 
 	void Sprite::_SetFrame()
 	{
 		_vertexArray.clear();
 
-		std::array<sf::Vertex, 4> vertices;
+		std::array<sf::Vertex, 4> vertices = transform::GetVertices(sf::FloatRect(Vector2::Zero(), _frameSize));
 		texture::SetTextureCoord(vertices, _framePosition, _frameSize);
 
-		vertices[0].position = { 0, 0 };
-		vertices[1].position = { (float)_frameSize.x, 0 };
-		vertices[2].position = _frameSize;
-		vertices[3].position = { 0, (float)_frameSize.y };
-
 		for (sf::Vertex& vertex : vertices)
+		{
+			vertex.color = _color;
 			_vertexArray.append(vertex);
+		}
 	}
 }
--- Engine/Graphics/Sprite.h
+++ Engine/Graphics/Sprite.h
@@ -17,21 +17,30 @@ namespace maoutch
 	public:
 		Sprite(const sf::Texture* texture, const Vector2i& frameSize, const Vector2i& framePosition);
 		virtual ~Sprite();
-		
+
+		// Setters
+
+		void SetColor(const sf::Color& color);
 		void SetFramePosition(const Vector2i& framePosition);
 		void SetFrameSize(const Vector2i& frameSize);
 
+		// Getters
+
+		sf::Color GetColor() const;
+
 		void Draw(sf::RenderWindow& window, const sf::Transform& transform);
+
+		sf::FloatRect GetBounds() const override;
 
 	protected:
 		sf::RenderStates _renderState;
 		sf::VertexArray _vertexArray;
 
 		Vector2i _frameSize;
+		Vector2i _framePosition;
+		sf::Color _color;
 
 	private:
-		Vector2i _framePosition;
-
 		virtual void _SetFrame();
 	};
 }
